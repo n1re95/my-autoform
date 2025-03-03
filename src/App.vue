a6b1a4354fc4945590a248e9a5d4564fd6daf044
n1re95 <ananyev95@gmail.com>
2025-03-03 21:29:18 +0300

    done container

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Button } from './components/ui/button'
+</script>
+
+<template>
+  <div class="app-container">
+    <div class="form-container">
+      <Button>testShit</Button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+  background-color: gray;
+  width: 100%;
+  height: 100vh;
+}
+.form-container {
+  width: 500px;
+  margin: auto;
+}
+</style>
